025b33fa3fdb6ae5eaa472015b1e0615b2f935a4
Cynthia5621 <muthonicynthia095@gmail.com>
2024-09-25 21:57:16 +0300

    Add files via upload

---
+++ Simple Interest.c
@@ -0,0 +1,12 @@
+//calculation of Simple Interest
+#include <stdio.h>
+ int main(){
+	//declaration and initialisation;
+	double p = 60000;
+	double t = 2;
+	double r = 20;
+	double interest = (p*r*t)/100;
+	printf(" The interest(double )is %lf", interest);
+	return 0;
+}
+
